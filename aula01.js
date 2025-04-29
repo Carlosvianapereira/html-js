0aa8c5e04c642829f733a06befd5826fdff3409a
CARLOS ALBERLANIO VIANA PEREIRA <carlospereira6662@digitalcollege.internal>
2025-04-29 20:17:50 -0300

    variaveis

---
+++ aula01.js
@@ -0,0 +1,11 @@
+//criação de variavel (const, let, var)
+const n1 = 10;
+const n2 = 40;
+
+
+//soma
+const resultado = n1 + n2
+
+
+//exibir resultado da soma
+//console.log(resultado)
---
+++ tipos_dados.js
@@ -0,0 +1,33 @@
+//var, const e let
+
+//1-const (constante)
+const pi = 3.14;
+
+//imprimindo a variavel no console
+//console.log(pi);
+
+//2-let (valor da variavel pode ser alterado)
+let nome = 'Viana'
+console.log(nome);
+
+nome = 'Viana Racing'
+console.log(nome);
+
+let desconto = 20.5
+
+console.log(desconto * 2);
+
+//
+desconto = desconto * 2
+console.log('Desconto atualizado: ' + desconto)
+
+//Array (lista)
+let tamanhos = ['P','PP','M','G', 36, 38, 40, 42];
+
+let temPet = false;
+
+const idade = 28;
+
+let dataCadastro = new Date();//pega a data e hora atual
+console.log(dataCadastro);
+
